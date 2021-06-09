2fe312c1cf38c1ebd97e08539b836bcac1c93c4b
michsantiago27 <michsantiago27@hotmail.com>
2021-06-09 09:34:52 -0500

    Commit para chambear en la de la oficina

---
+++ Módulo 5 - React y Bootstrap/4. React Ejercicio - Instagram/instagram-react-app/src/seccion-1/Top.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Top extends React.Component {
+    render() {
+        return (
+            <div className='btn bg-color-red'>Botonazo</div>
+        )
+    }
+}
+
+export default Top;
