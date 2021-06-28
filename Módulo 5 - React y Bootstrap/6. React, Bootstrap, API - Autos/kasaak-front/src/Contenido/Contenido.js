f8b2d2c607a9874cc8509977d72977139f5d374d
michsantiago27 <michsantiago27@hotmail.com>
2021-06-28 18:35:17 -0500

    Commit antes de regresar a clases

--- Módulo 5 - React y Bootstrap/6. React, Bootstrap, API - Autos/kasaak-front/src/Contenido/Contenido.js
+++ Módulo 5 - React y Bootstrap/6. React, Bootstrap, API - Autos/kasaak-front/src/Contenido/Contenido.js
@@ -2,69 +2,134 @@ import React from 'react';
 import './Contenido.css';
 const fetch = require("node-fetch"); // // 1) Para cada proyecto hay que instalar el paquete con 'npm install node-fetch' (para cada proyecto) e importar fetch
 
-function Tarjeta() {
-    return(
-        <div className='col g-2'>
-            <div className='card tarjeta'>
-                <div className='card-body'>
-                    <h5 className='card-title-left'>Onix</h5>
-                    <p className='card-subtitle text-muted'>Rock</p>
-                </div>
-                <img className='card-image-top' src='https://picsum.photos/300/200' alt='no alternative bitch' />
-                <div className='card-body'>
-                    <p className='card-text'>Descripción</p>
-                    <div class="d-grid">
-                        <div className='row justify-content-between'>
-                            <div className='col-auto'>
-                                <button class="btn btn-dark btn-sm">Ver más</button>
-                            </div>
-                            <div className='col-7'>
-                                <button class="btn btn-light btn-sm">Agregar a pokedex</button>
-                            </div>
+class PokeTarjeta extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            // Variables de control del componente
+            error: null,
+            isLoaded: false,
+            // Url del pokemon que se pintará 
+            pokeUrl: props.url,
+            // Variables del pokemon que se pintarán
+            pokeNombre: props.nombre,
+            pokeNumero: null,
+            pokeTipo: null,
+            pokeImagen: null,
+            pokeDetalle: null,
+        }
+    }
+    componentDidMount(){
+        fetch(this.state.pokeUrl)
+        .then(x=>x.json())
+        .then(x=> {
+            this.setState({
+                isLoaded: true,
+                pokeNumero: x.id,
+                pokeTipo: x.types[0].type.name,
+                pokeImagen: x.sprites.front_default,
+            });
+        },
+        error => {
+            this.setState({
+                isLoaded: true,
+                error
+            })
+        })  
+    }
+    render() {
+        if (this.state.error) {
+            return(
+                <div>Error en carga de componentes</div>
+            );
+        }
+        else if(!this.state.isLoaded){
+            return (
+                <div className='col g-2'>
+                    <div className='card tarjeta'>
+                        <div className='cargandoTarjeta'>
+                            <img src='./pokeball-loading'></img>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    ); 
+            );
+        } else {
+            return(
+                <div className='col g-2'>
+                    <div className='card tarjeta'>
+                        <div className='card-body'>
+                            <h5 className='card-title-left tarjetaTitulo'>
+                                <div>{this.state.pokeNombre}</div>
+                                <div className='text-muted'>#{this.state.pokeNumero}</div>
+                            </h5>
+                            <p className='card-subtitle text-muted'>{this.state.pokeTipo}</p>
+                        </div>
+                        <img className='card-image-middle' src={this.state.pokeImagen} alt='no alternative bitch' />
+                        <div className='card-body tarjetaBotones'>
+                            <button className="btn btn-dark btn-sm">Ver más</button>
+                            <button className="btn btn-light btn-sm">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
 
 class Contenido extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {pokemon: []};
+        this.state = {
+            error: null,
+            isLoaded: false,
+            pokeTarjetas: null
+        };
     }
-    
-    pintarGrid() {
-
-        fetch('https://pokeapi.co/api/v2/pokemon/raichu')
-        .then(response => response.json()) 
-        .then((data) => {
-            this.setState({pokemon : data.types[0].type.name});
-            console.log('1. Tipo de Raichu: ' + data.types[0].type.name);
+    componentDidMount() {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(x=>x.json())
+        .then(x=>{
+            var pokeObjs = [];
+            x.results.forEach((poke,index)=>{
+                pokeObjs.push(<PokeTarjeta 
+                    key={index}
+                    url={poke.url}
+                    nombre={poke.name}
+                />);
+            });
+            this.setState({
+                isLoaded: true,
+                pokeTarjetas: pokeObjs
+            });
+        },
+        error => {
+            this.setState({
+                isLoaded: true,
+                error
+            })
         })
-        .catch(console.log('San querida, hubo un error en el consumo de la API'));
-
-        var pokegrid = this.state.pokemon;
-        pokegrid.map(poke => {
-            return (
-                <Tarjeta 
-                    key={poke.toString()}
-                />
-            );
-        })
-
-        return pokegrid;
     }
-    
     render() {
-        return (
-        <div className='container-fluid contenido'>
-            <div className='row g-2'>  {/* Se puede usar solo una row y las columnas que se pasen se wrapean a la siguiente fila*/ }
-                {this.pintarGrid()}
+        if (this.state.error) {
+            return (
+                <div>Error en cara de pokeGrid</div>
+            )
+        } else if(!this.state.isLoaded) {
+            return (
+                <div className='container-fluid contenido'>
+                    <img src='./pokeball-loading'></img>
+                </div>
+            );
+        } else {
+            return (
+            <div className='container-fluid contenido'>
+                <div className='row g-2'>  {/* Se puede usar solo una row y las columnas que se pasen se wrapean a la siguiente fila*/ }
+                    {this.state.pokeTarjetas}
+                </div>
             </div>
-        </div>
-        );
+            );
+    
+        }
     }
 }
 
